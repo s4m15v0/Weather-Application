5c795b42e2124ea5d637b268f8d7361e196e70d6
s4m15v0 <shamiulislam15@gmail.com>
2022-01-05 20:29:11 +0600

    js file updated

--- main.js
+++ main.js
@@ -1,176 +1,180 @@
-//data -
-//R(Read)
-//UI-CRUD
-//LocalStorage- CRUD
-
-//Application development workflow
-//Requirements(get weather data from API and display in UI)
-//Design
-//code
-
-const storage = {
-    city: '',
-    country: '',
-    saveItem() {
-        localStorage.setItem('wetherApp-city', this.city)
-        localStorage.setItem('wetherApp-country', this.country)
+const weatherStore = {
+    privateCity: 'rajshahi',
+    privateCountry: 'BD',
+    API_kEY: '008023412376865840e32be1c8423c5d',
+    set city(name) {
+        console.log('city name')
+        //validation 
+        this.privateCity = name
     },
-    getItem() {
-        this.city = localStorage.getItem('wetherApp-city')
-        this.country = localStorage.getItem('wetherApp-country')
-    }
+    set country(name) {
+        console.log('country name')
+        this.privateCountry = name
+    },
+    async fetchData() {
+        const res = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.privateCity},${this.privateCountry}&units=metric&appid=${this.API_kEY}`)
+        return await res.json()
+
+    },
 }
 
-///object property, method
-const weatherData = {
-    city: '',
-    country: '',
-    API_KEY: 'd777b9cd1436c98f958dac1e189e4f09',
-    async getWeather() {
-        try {
-            const res = await fetch(
-                `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&units=metric&appid=${this.API_KEY}`
-            )
-            const data = await res.json()
-
-            return {
-                data
-            }
-        } catch (err) {
-            console.log(err.message)
-        }
+const storage = {
+    privateCity: '',
+    privateCountry: '',
+    set city(name) {
+        this.privateCity = name
+    },
+    set country(name) {
+        this.privateCountry = name
+    },
+    saveItem() {
+        localStorage.setItem('BD-weather-city', this.privateCity)
+        localStorage.setItem('BD-weather-country', this.privateCountry)
     }
 }
 
 const UI = {
     loadSelectors() {
-        const cityElm = document.querySelector('#city')
-        const cityInfoElm = document.querySelector('#w-city')
-        const iconElm = document.querySelector('#w-icon')
-        const temperatureElm = document.querySelector('#w-temp')
-        const pressureElm = document.querySelector('#w-pressure')
-        const humidityElm = document.querySelector('#w-humidity')
-        const feelElm = document.querySelector('#w-feel')
-        const formElm = document.querySelector('#form')
-        const countryElm = document.querySelector('#country')
-        const messageElm = document.querySelector('#messageWrapper')
+        const cityElm = document.querySelector("#city")
+        const cityInfoElm = document.querySelector("#w-city")
+        const iconElm = document.querySelector("#w-icon")
+        const temperatureElm = document.querySelector("#w-temp")
+        const pressureElm = document.querySelector("#w-pressure")
+        const humidityElm = document.querySelector("#w-humidity")
+        const feelElm = document.querySelector("#w-feel")
+        const formElm = document.querySelector("#form")
+        const countryElm = document.querySelector("#country")
+        const messageElm = document.querySelector("#messageWrapper")
 
         return {
-            cityInfoElm,
             cityElm,
-            countryElm,
+            cityInfoElm,
             iconElm,
             temperatureElm,
             pressureElm,
-            feelElm,
             humidityElm,
+            feelElm,
             formElm,
-            messageElm
+            countryElm,
+            messageElm,
+
         }
     },
-    showMessage(msg) {
-        const { messageElm } = this.loadSelectors()
-        const elm = `<div class='alert alert-danger' id='message'>${msg}</div>`
-        messageElm.insertAdjacentHTML('afterbegin', elm)
-        this.hideMessage()
-    },
-    hideMessage() {
-        const messageElm = document.querySelector('#message')
-        setTimeout(() => {
-            if (messageElm) {
-                messageElm.remove()
-            }
-        }, 2000)
-    },
-    validateInput(city, country) {
-        if (country === '' || city === '') {
-            this.showMessage('Please provide valid country and city')
-            return false
-        } else {
-            return true
-        }
-    },
+
     getInputValues() {
         const { cityElm, countryElm } = this.loadSelectors()
-        const country = countryElm.value
         const city = cityElm.value
+        const country = countryElm.value
 
-        const isValid = this.validateInput(city, country)
-
-        if (isValid) {
-            //setting city and country to weatherData
-            weatherData.city = city
-            weatherData.country = country
-
-            //setting city and country to localStorage
-            storage.city = city
-            storage.country = country
-
-            //saving city and country localStorage
-            storage.saveItem()
+        return {
+            city,
+            country,
         }
     },
-    getIcon(iconCode) {
+
+    validateInput(city, country) {
+        let error = false
+        if (city === '' || country === '') {
+            error = true
+        }
+        return error
+    },
+
+    hideMessage() {
+        const msgContentElm = document.querySelector('.err-msg')
+        if (msgContentElm) {
+            setTimeout(() => {
+                msgContentElm.remove()
+            }, 2000)
+
+        }
+    },
+
+
+    showMessage(msg) {
+        const { messageElm } = this.loadSelectors()
+        const elm = `<div class = 'alert alert-danger err-msg'>${msg}</div>`
+        const msgContentElm = document.querySelector('.err-msg')
+        if (!msgContentElm) {
+            messageElm.insertAdjacentHTML('afterbegin', elm)
+        }
+
+        this.hideMessage()
+    },
+
+    getIconSrc(iconCode) {
         return 'https://openweathermap.org/img/w/' + iconCode + '.png'
     },
-    printWeather(weatherData) {
-        const { main, weather, name: cityName } = weatherData
 
+    printWeather(data) {
         const {
             cityInfoElm,
+            iconElm,
             temperatureElm,
             pressureElm,
             humidityElm,
             feelElm,
-            iconElm
         } = this.loadSelectors()
-        //paint to UI
-        cityInfoElm.textContent = cityName
-        temperatureElm.textContent = `Temperature: ${main.temp}°C`
-        pressureElm.textContent = `Pressure: ${main.pressure}Kpa`
-        humidityElm.textContent = `Humidity ${main.humidity}`
+        const { main, weather, name } = data
+        console.log(main)
+        cityInfoElm.textContent = name
+        temperatureElm.textContent = `temperature: ${main.temp}°C`
+        humidityElm.textContent = `Humidity: ${main.humidity}kpa`
+        pressureElm.textContent = `pressure: ${main.pressure}kpa`
         feelElm.textContent = weather[0].description
-        iconElm.setAttribute('src', this.getIcon(weather[0].icon))
+        const src = this.getIconSrc(weather[0].icon)
+        iconElm.setAttribute('src', src)
     },
-    resetInputValues() {
-        const { cityElm, countryElm } = this.loadSelectors()
+
+    resetInput() {
+        const { countryElm, cityElm } = this.loadSelectors()
         cityElm.value = ''
         countryElm.value = ''
     },
-    handleRemoteData() {
-        //get remote data
-        const { data } = await weatherData.getWeather()
 
-        if (data.cod === '404') {
-            UI.showMessage(data.message)
-        } else {
-            UI.printWeather(data)
-        }
-    },
     init() {
         const { formElm } = this.loadSelectors()
-        formElm.addEventListener('submit', async e => {
+        formElm.addEventListener("submit", async (e) => {
             e.preventDefault()
-            //get the values from input
-            this.getInputValues()
-            //reset Input values
-            this.resetInputValues()
-            this.handleRemoteData()
+            //get the input values
+            const { city, country } = this.getInputValues()
+            //reset input
+            this.resetInput()
+            //validate input
+            const error = this.validateInput(city, country)
+            //show error message in UI
+            if (error) return this.showMessage('please provide valid input')
+
+            //setting data to weather data store
+            weatherStore.city = city
+            weatherStore.country = country
+            //setting to local storage
+            storage.city = city
+            storage.country = country
+            storage.saveItem()
+            //send request to API server
+            const data = await weatherStore.fetchData()
+            this.printWeather(data)
         })
 
-        window.addEventListener('DOMContentLoaded', async () => {
-            storage.getItem()
-            //city and country from storage
-            const city = storage.city
-            const country = storage.country
+        document.addEventListener('DOMContentLoaded', async (e) => {
+            //load data from local storage
+            if (localStorage.getItem('BD-weather-city')) {
+                //setting data to data store
+                weatherStore.city = localStorage.getItem('BD-weather-city')
+            }
 
-            //data source update(if there is no data in localStorage setting default city and country)
-            weatherData.city = city ? city : 'Pabna'
-            weatherData.country = country ? country : 'BD'
+            if (localStorage.getItem('BD-weather-country')) {
+                //setting data to data store
+                weatherStore.country = localStorage.getItem('BD-weather-country')
+            }
+            //send request to API server
+            const data = await weatherStore.fetchData()
+            //shaow data to UI
+            this.printWeather(data)
 
-            this.handleRemoteData()
         })
-    }
+
+    },
 }
-
 UI.init()
